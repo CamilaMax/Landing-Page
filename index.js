ae9d4422f514ecc3b236730f243ea760d68c9e52
Camila Maximo <camilamaximo20@gmai.com>
2023-03-25 16:24:31 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+
+/* Primeiro javascript criado */
+
+
+document.querySelector("#botao-MeAdicione").addEventListener("click", function(){
+    
+    window.alert('Gratidão!!! Agora você faz parte da nossa comunidade!');
+    window.alert('Breve bateremos um bom papo!')
+})
